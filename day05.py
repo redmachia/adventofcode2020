00eb0708eb7bfdea1a5289e2772a662c5a448770
Red Machia <13063136+redmachia@users.noreply.github.com>
2020-12-09 05:55:37 +0100

    Update day05.py

--- day05.py
+++ day05.py
@@ -1,37 +1,3 @@
-from math import ceil, floor
-"""
-# OLD VERSION
-def part01(passess):
-    max = 127
-    min = 0
-    max_c = 7
-    min_c = 0
-    
-    for letter in passess:
-        if letter == 'F':
-            max = floor((max - min)/2) + min
-        elif letter == 'B':
-            min = ceil((max-min) / 2) + min
-        elif letter == 'L':
-            max_c = floor((max_c - min_c) / 2) + min_c
-        else:
-            min_c = ceil((max_c-min_c) / 2) + min_c
-    return (min * 8) + max_c
-
-def part02(seats):
-    for seat in range(min(seats), max(seats)):
-        if ((seat - 1) in seats) and ((seat not in seats)) and ((seat + 1) in seats):
-            return seat
-
-with open('.\\Inputs\\day05input.txt', 'r') as f:
-    seats = list()
-    for line in f:
-        seats.append(part01(line.strip('\n')))
-    print(f"Solution to part 1: {max(seats)}")
-    my_seat = part02(seats)
-    print(f"Solution to part 2: {my_seat}")
-
-"""
 with open('.\\Inputs\\day05input.txt', 'r') as f:
     # New version
     trans = str.maketrans('FBRL', '0110')
