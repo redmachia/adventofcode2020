9d32604e4697f37258aaf623c9553e0a771a470e
Red Machia <13063136+redmachia@users.noreply.github.com>
2020-12-09 06:01:13 +0100

    Update day08.py

--- day08.py
+++ day08.py
@@ -17,22 +17,17 @@ def runProg(prog, swap = False):
             op_ptr += 1
     return acc
 
-def tryRun(code_b, i):
-    code = code_b.copy()
-    if code[i].startswith('jmp'):
-        code[i] = code[i].replace('jmp', 'nop')
-    elif code[i].startswith('nop'):
-        code[i] = code[i].replace('nop', 'jmp')
-    return runProg(code, True)
-    
-
 with open('.\\Inputs\\day08input.txt', 'r') as f:
     code = f.readlines()
     # Part A:
     print(runProg(code))
     # Part B:
     for i in range(0, len(code)):
-        ret = tryRun(code, i)
+        trans = str.maketrans('jmpnop', 'nopjmp')
+        # Swap opcode
+        code[i] = code[i].translate(trans)
+        ret = runProg(code, True)
+        # Swap it back if test run fails
+        code[i] = code[i].translate(trans)
         if ret:
             print(ret)
-
